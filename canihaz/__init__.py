b3b2384739837d70d1d91cdcaba1ddca2a1543c3
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-01 16:39:45 -0300

    Separate requests logic

--- canihaz/__init__.py
+++ canihaz/__init__.py
@@ -8,24 +8,31 @@ import typer
 __version__ = "0.1.0"
 
 
+def isavailable(name: str) -> bool:
+    url = f"https://pypi.org/project/{name}/"
+    response = requests.get(url)
+    if response.status_code == 404:
+        return True
+    elif response.status_code == 200:
+        return False
+    else:
+        typer.secho(
+            f"An error occurred: {response.status_code}",
+            fg=typer.colors.BRIGHT_RED,
+        )
+        raise typer.Exit(response.status_code)
+
+
 def app(names: List[str]):
     for name in names:
-        url = f"https://pypi.org/project/{name}/"
-
-        response = requests.get(url)
-        if response.status_code == 404:
+        if isavailable(name):
             typer.secho(name, nl=False, fg=typer.colors.BRIGHT_MAGENTA)
             typer.echo(" is available on PyPI 🎉")
-        elif response.status_code == 200:
+        else:
             typer.secho(name, nl=False, fg=typer.colors.BRIGHT_MAGENTA)
             typer.echo(" is ", nl=False)
             typer.secho("not available", nl=False, fg=typer.colors.BRIGHT_RED)
             typer.echo(" on PyPI")
-        else:
-            typer.secho(
-                f"An error occurred: {response.status_code}",
-                fg=typer.colors.BRIGHT_RED,
-            )
 
 
 def main():
